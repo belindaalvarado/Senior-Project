3d98f3c72fb15ef9b4cd15ff4dd517d17538349e
belindaalvarado <alvaradobelinda653@gmail.com>
2023-09-14 16:48:26 -0500

    added the fonts and font files

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Lobster Two',
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
